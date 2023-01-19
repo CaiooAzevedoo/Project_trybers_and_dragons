d643168fb9baafad657917e53df77ead19a02a73
Caio <caiusito@gmail.com>
2023-01-19 01:03:32 -0300

    Desenvolvendo req 7

--- src/Character.ts
+++ src/Character.ts
@@ -29,6 +29,96 @@ class Character implements Fighter {
       amount: getRandomInt(1, 10),
     };
   }
+
+  public get race(): Race {
+    return this._race;
+  }
+
+  // public set race(value: Race) {
+  //   this._race = value;
+  // }
+
+  public get archetype(): Archetype {
+    return this._archetype;
+  }
+
+  // public set archetype(value: Archetype) {
+  //   this._archetype = value;
+  // }
+
+  public get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  // public set maxLifePoints(value: number) {
+  //   this._maxLifePoints = value;
+  // }
+
+  public get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  // public set lifePoints(value: number) {
+  //   this._lifePoints = value;
+  // }
+
+  public get strength(): number {
+    return this._strength;
+  }
+
+  // public set strength(value: number) {
+  //   this._strength = value;
+  // }
+
+  public get defense(): number {
+    return this._defense;
+  }
+
+  // public set defense(value: number) {
+  //   this._defense = value;
+  // }
+
+  public get dexterity(): number {
+    return this._dexterity;
+  }
+
+  // public set dexterity(value: number) {
+  //   this._dexterity = value;
+  // }
+
+  public get energy(): Energy {
+    return this._energy;
+  }
+
+  // public set energy(value: Energy) {
+  //   this._energy = value;
+  // }
+
+  receiveDamage(attackPoints: number) {
+    const damage = attackPoints - this.defense;
+
+    if (damage > 0) this._lifePoints -= damage;
+    else this._lifePoints -= 1;
+    if (this._lifePoints <= 0) this._lifePoints = -1;
+
+    return this._lifePoints;
+  }
+
+  attack(enemy: Fighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+
+  levelUp(): void {
+    this._maxLifePoints = getRandomInt(1, 10);
+    this._strength = getRandomInt(1, 10);
+    this._dexterity = getRandomInt(1, 10);
+    this._defense = getRandomInt(1, 10);
+    this._energy.amount = 10;
+    this._lifePoints = this._maxLifePoints;
+    if (this.maxLifePoints > this.race.maxLifePoints) {
+      this._maxLifePoints = this._race.maxLifePoints;
+    }
+  }
 }
 
 export default Character;
